f54e3beb3ae290468ca8eed1712b27e84bffcd55
Izver-stack <122816170+Izver-stack@users.noreply.github.com>
2023-03-07 22:07:55 +0300

    pr2 1

---
+++ PR1/1-4/4/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Car vaz2115 = new Car();
+vaz2115.name = "Ваз 2115";
+vaz2115.speed = 100;
+vaz2115.Print();
+
+Car priora = new Car();
+priora.name = "Лада Приора";
+priora.speed = 140;
+priora.Print();
+
+Car astra = new Car();
+astra.name = "Опель Астра";
+astra.speed = 190;
+astra.Print();
+
+Car lamba = new Car();
+lamba.name = "Ламборгини урус";
+lamba.speed = 40;
+lamba.Print();
+
+Console.WriteLine($"гоночки");
+if (vaz2115.speed> priora.speed)
+{
+    Console.WriteLine($"ВАЗ 21115 пришел первым");
+}
+
+if (priora.speed> astra.speed)
+{
+    Console.WriteLine($"Приора пришла второй");
+}
+
+if (astra.speed> lamba.speed)
+{
+    Console.WriteLine($"ВАЗ 21115 пришла третьей, а ламба последней");
+}
+
+class Car
+{
+    public string name;
+    public int speed;
+    public void Print()
+    {
+        Console.WriteLine($"Название автомобиля: {name}");
+        Console.WriteLine($"Максимальная скорость: {speed} км/ч");
+    }
+}
+
---
+++ PR2/ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿
+solveSqrtExpression solveSqrt = new solveSqrtExpression();
+void sqrtExpression(int a, int b, int c)
+{
+    solveSqrt.setDiscr(a, b, c);
+    solveSqrt.setSqrt1(a, b, c);
+    solveSqrt.setSqrt2(a, b, c);
+}
+
+//sqrtExpression(9, -6, 2); // нет корней
+sqrtExpression(-1, 7, 8); // -1, 8
+//sqrtExpression(2, 5, -7); // 
+
+Console.WriteLine("Дискриминант  " + solveSqrt.getDiscr());
+if (solveSqrt.getSqrt1() == -999999999 && solveSqrt.getSqrt2() == -999999999)
+{
+    Console.WriteLine("Корней нет");
+}
+else if (solveSqrt.getSqrt1() == -999999999 || solveSqrt.getSqrt2() == -999999999)
+{
+    if (solveSqrt.getSqrt1() == -999999999)
+    {
+        Console.WriteLine("Корень 1: Корня нет");
+    }
+    else
+    {
+        Console.WriteLine("Корень 2: Корня нет");
+    }
+}
+else
+{
+    Console.WriteLine("Корень 1  " + solveSqrt.getSqrt1());
+    Console.WriteLine("Корень 2  " + solveSqrt.getSqrt2());
+}
+
+Console.ReadKey();
+
+class solveSqrtExpression
+{
+    private double sqrt1, sqrt2, discr;
+
+    public double getSqrt1()
+    {
+        return sqrt1;
+    }
+    public double getSqrt2()
+    {
+        return sqrt2;
+    }
+    public double getDiscr()
+    {
+        return discr;
+    }
+    public void setSqrt1(int a, int b, int c)
+    {
+        this.sqrt1 = CalculateRoots(a, b, getDiscr())[0];
+    }
+    public void setSqrt2(int a, int b, int c)
+    {
+        this.sqrt2 = CalculateRoots(a, b, getDiscr())[1];
+    }
+    public void setDiscr(int a, int b, int c)
+    {
+        this.discr = (b * b) - 4 * a * c;
+    }
+    private double[] CalculateRoots(int a, int b, double d)
+    {
+        if (d > 0)
+        {
+            double sqrt1 = (-b + Math.Sqrt(d)) / (2 * a);
+            double sqrt2 = (-b - Math.Sqrt(d)) / (2 * a);
+            double[] sqrts = { sqrt1, sqrt2 };
+            return sqrts;
+        }
+        else if (d == 0)
+        {
+            double sqrt1 = (-b + Math.Sqrt(d)) / (2 * a);
+            double[] sqrts = { sqrt1, -999999999 };
+            return sqrts;
+        }
+        else
+        {
+            double[] sqrts = { -999999999, -999999999 };
+            return sqrts;
+        }
+    }
+}
