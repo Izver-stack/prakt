38e41165a269e6fedf1d17358234dc013101373d
Izver-stack <122816170+Izver-stack@users.noreply.github.com>
2023-03-09 20:17:04 +0300

    1-6

---
+++ PR3/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿class SmsMessage
+{
+    private string MessageText { get; set; }
+    private double Price { get; set; }
+
+
+    public SmsMessage(string messageText)
+    {
+        MessageText = messageText;
+        Price = 0;
+    }
+
+    public void CalculatePrice()
+    {
+        if (MessageText.Length <= 65)
+        {
+            Price = 150;
+        }
+        else
+        {
+            Price = (150 + (MessageText.Length - 65) * 50) / 100;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Сообщение: {MessageText}, Цена: {Price}";
+    }
+
+    public static void Main(string[] args)
+    {
+        SmsMessage message = new SmsMessage("Как на счет того, что бы сдать курсовую работу вовремя?");
+        message.CalculatePrice();
+        Console.WriteLine(message);
+    }
+}
---
+++ PR3/ConsoleApp6/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+Random rand = new Random();
+rand.GetIntFixedLength(8);
+
+public static class RandExtension
+{
+    public static int GetIntFixedLength(this Random rand, int length)
+    {
+        length = length > 0 && length < 10 ? length : 1; 
+        return rand.Next(Convert.ToInt32(Math.Pow(10, length - 1)),
+                         Convert.ToInt32(Math.Pow(10, length)));
+
+        
+
+
+    }
+
+    static void Calc()//дисперсия
+    {
+
+        int N = 0;
+        double sum;
+        int myN;
+        int i, j, k;
+        double[] avr;
+        avr = new double[N];
+
+        for (i = 1; i <= N / 2; i++)
+        {
+            myN = N / i;
+            for (j = 0; j <= myN - 1; j++)
+            {
+                sum = 0;
+                for (k = j * i; k < j * i + i; k++)
+                {
+                    sum += k;
+                }
+                avr[j] = sum / i;
+            }
+            sum = 0;
+            for (j = 0; j < (myN - 1); j++)
+            {
+                sum += Math.Pow((avr[j + 1] - avr[j]), 2);
+            }
+            
+        }
+
+    }
+    public static double StandardDeviation(List<double> valueList)//среднеквадратическое отклонение
+    {
+        double M = 0.0;
+        double S = 0.0;
+        int k = 1;
+        foreach (double value in valueList)
+        {
+            double tmpM = M;
+            M += (value - tmpM) / k;
+            S += (value - tmpM) * (value - M);
+            k++;
+        }
+        return Math.Sqrt(S / (k - 2));
+    }
+    
+}
+
