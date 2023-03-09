10fdba854cce1b3abc40f1cb0b16e8fb3232829d
Izver-stack <122816170+Izver-stack@users.noreply.github.com>
2023-03-09 18:26:23 +0300

    1-3

---
+++ PR2/ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿Operation n = new Operation();
+n.predictNumber();
+while (true)
+{
+    int input = Convert.ToInt32(Console.ReadLine());
+    if (n.setNumber(input))
+    {
+        n.predictNumber();
+    }
+    else
+    {
+        Console.WriteLine("Вы ошиблись");
+        break;
+    }
+}
+
+
+class Operation
+{
+    private int n = 0;
+
+    public int getNumber()
+    {
+        return n;
+    }
+
+    public void resetNumber()
+    {
+        n = 0;
+    }
+
+    public bool setNumber(int n)
+    {
+        if (this.n + 1 == n)
+        {
+            this.n = n;
+            return true;
+        }
+        else
+        {
+            resetNumber();
+            return false;
+        }
+    }
+
+    public void predictNumber()
+    {
+        Console.WriteLine("Введите число " + (this.n + 1));
+    }
+}
+
+
