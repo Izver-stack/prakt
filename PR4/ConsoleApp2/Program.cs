05cbdcc72f7fedfd0c2edb927be5dd77aca9327d
Izver-stack <122816170+Izver-stack@users.noreply.github.com>
2023-03-11 21:01:12 +0300

    праки

---
+++ PR4/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace zadacha_2
+{
+    class Package
+    {
+        public string Description { get; private set; }
+        public int Weight { get; private set; }
+        public Package(string description, int weight)
+        {
+            Description = description;
+            Weight = weight;
+        }
+    }
+    class Otpravka
+    {
+        private const int Limit = 20;
+        private static int LimitWeight;
+
+        public void SendPackage(Package package)
+        {
+            LimitWeight += package.Weight;
+            if (LimitWeight >= Limit)
+            {
+                Console.WriteLine(
+                    "Ошибка! Вес отправленных посылок превысил лимит, отправка прервана.");
+            }
+            else
+            {
+                Console.WriteLine("{0} весом {1} кг успешно отправлена.",
+                                  package.Description, package.Weight);
+            }
+        }
+    }
+    class mail
+    {
+        static void Main(string[] args)
+        {
+            Package package_1 = new Package("Легкая посылка #1", 5);
+            Package package_2 = new Package("Тяжелая посылка #2", 10);
+            Otpravka sendingService = new Otpravka();
+            sendingService.SendPackage(package_1);
+            sendingService.SendPackage(package_2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PR4/ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace zadacha_3
+{
+    class Orc
+    {
+        private static int Count;
+        public static int totalGolD { get; private set; }
+        public int StealGolD { get; private set; }
+        public Orc()
+        {
+            Count++;
+            totalGolD += 2;
+            if (Count > 5)
+            {
+                StealGolD += 2;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orc orc_1 = new Orc();
+            Console.WriteLine(Orc.totalGolD);
+            Orc orc_2 = new Orc();
+            Orc orc_3 = new Orc();
+            Console.WriteLine(Orc.totalGolD);
+            Orc orc_4 = new Orc();
+            Orc orc_5 = new Orc();
+            Orc orc_6 = new Orc();
+            Console.WriteLine(orc_6.StealGolD);
+            Console.ReadKey(true);
+        }
+    }
+}
